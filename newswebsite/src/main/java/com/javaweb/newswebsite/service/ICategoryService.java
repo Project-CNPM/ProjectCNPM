07e77a95cd9d94c44c9cc565c50809e3ddb04f60
maibatan <maibatan@github.com>
2020-11-18 08:13:32 +0700

    Revert "them xoa sua danh muc Done"
    This reverts commit b9d08654ee8b04cd779c58ddfe75f078d2300eda.

--- newswebsite/src/main/java/com/javaweb/newswebsite/service/ICategoryService.java
+++
@@ -1,9 +0,0 @@
-package com.javaweb.newswebsite.service;
-
-import com.javaweb.newswebsite.dto.CategoryDTO;
-
-public interface ICategoryService {
-	CategoryDTO save(CategoryDTO categoryDto);
-	void delete(Long[] ids);
-
-}
