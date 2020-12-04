4707bc2b5b79a05e530fe1eddf322cc850d411ff
Ngọc Sang <you@example.com>
2020-12-04 22:14:01 +0700

    thongke

--- newswebsite/src/main/java/com/javaweb/newswebsite/api/UserAPI.java
+++ newswebsite/src/main/java/com/javaweb/newswebsite/api/UserAPI.java
@@ -1,5 +1,23 @@
 package com.javaweb.newswebsite.api;
 
 public class UserAPI {
-
+	@GetMapping(value = "/user/statistics")
+	public UserOutPut statistics(@RequestParam("startdate") String startdate,
+								 @RequestParam("enddate") String enddate,
+								 HttpServletRequest request) throws java.text.ParseException {
+		UserOutPut userOut = new UserOutPut();
+		SimpleDateFormat fomater = new SimpleDateFormat("yyyy-MM-dd");
+		Date sDate = null;
+		Date eDate = null;
+		
+		try {
+			sDate = fomater.parse(startdate);
+			eDate = fomater.parse(enddate);
+		}catch(ParseException e) {
+			e.printStackTrace();
+		}
+		userOut.setListUser(service.findAllByCreatedDateBetween(sDate, eDate));
+		userOut.setTotalUser(userOut.getListUser().size());
+		return userOut;
+	}
 }
